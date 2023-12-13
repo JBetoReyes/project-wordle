959ae179dd68d06840f52ff9149ad469e1e82feb
JBetoreyes <josereyes55@hotmail.com>
2023-12-12 21:22:38 -0600

    adding form and input

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
 
 function GuessInput() {
-  return <div></div>;
+  const [guess, setGuess] = React.useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(guess);
+    setGuess("");
+  };
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter Guess:</label>
+      <input
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        id="guess-input"
+        type="text"
+        pattern="[a-zA-Z]{5,5}"
+        required
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
