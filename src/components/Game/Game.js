20c9a89638aef35ec74b379bce72fc971a05473b
JBetoreyes <josereyes55@hotmail.com>
2023-12-14 20:34:00 -0600

    adding banner to game, refactors addGuess to set game status

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,6 +6,7 @@ import { checkGuess } from "../../game-helpers";
 
 import GuessInput from "../GuessInput";
 import GuessResults from "../GuessResults";
+import Banner from "../Banner/Banner";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -13,14 +14,32 @@ const answer = sample(WORDS);
 function Game() {
   console.log(answer);
   const [guesses, setGuesses] = React.useState([]);
-  const addGuess = (guess) => {
-    const newGuess = checkGuess(guess, answer);
-    setGuesses((guesses) => [...guesses, newGuess]);
+  const [gameStatus, setGameStatus] = React.useState({
+    isWinner: false,
+    isLoser: false,
+  });
+  const addGuess = (rawGuess) => {
+    const newGuess = checkGuess(rawGuess, answer);
+    const newGuesses = [...guesses, newGuess];
+    setGuesses(newGuesses);
+    if (rawGuess === answer) {
+      setGameStatus({ ...gameStatus, isWinner: true });
+    } else if (newGuesses.length === 6) {
+      setGameStatus({ ...gameStatus, isLoser: true });
+    }
   };
+  const gameCompleted = gameStatus.isWinner || gameStatus.isLoser;
   return (
     <>
       <GuessResults guesses={guesses} />
-      <GuessInput addGuess={addGuess} />
+      <GuessInput disabled={gameCompleted} addGuess={addGuess} />
+      {gameCompleted ? (
+        <Banner
+          guesses={guesses}
+          isWinner={gameStatus.isWinner}
+          correctAnswer={answer}
+        />
+      ) : null}
     </>
   );
 }
